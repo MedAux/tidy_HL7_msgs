7e23ab5680699d501160a3b57fa48e5942945c12
Mike_Feyder <mfeyder@stellatechnology.com>
2017-09-22 11:58:54 -0400

    add unit tests

---
+++ test/test_query_raw.py
@@ -0,0 +1,56 @@
+import pytest
+from query_raw.query_raw import *
+from query_raw.test import test_data
+
+def test_lst_lens_equal():
+    with pytest.raises(RuntimeError):
+        check_lens_equal([1, 2, 3], [1, 2, 3, 4])
+
+def test_nested_lst_lens_equal():
+    with pytest.raises(RuntimeError):
+        check_nested_lens_equal(
+            [[1], [2], [3, 4]], 
+            [[1], [2], [3, 4, 5]]
+        )
+
+def test_flatten():
+    assert flatten([[1, 2], [3, 4]]) == [1, 2, 3, 4]
+    assert flatten([[1, 2, [3, 4]]]) == [1, 2, [3, 4]]
+    assert flatten([[1, 2], []]) == [1, 2]
+    assert flatten([]) == []
+
+def test_zip_nested_lsts():
+    assert zip_nested_lists([['a', 'b']], [['y', 'z']]) == (
+        [[('a', 'y'), ('b', 'z')]]
+    )
+    assert zip_nested_lists([['a', 'b'],['c', 'd']], [['w', 'x'],['y', 'z']]) == (
+        [[('a', 'w'), ('b', 'x')], [('c', 'y'), ('d', 'z')]]
+    )
+
+def test_concat_fields():
+    assert concat_fields([[['a', 'b']]]) == ['a', 'b']
+    assert concat_fields([[['a', 'b']], [['y', 'z']], [['s', 't']]]) == (
+        ['a,y,s', 'b,z,t']
+    )
+
+def test_parse_loc():
+    loc_d2 = parse_loc('PR1.3')
+    assert loc_d2['depth'] == 2
+    assert loc_d2['seg'] == 'PR1'
+    assert loc_d2['comp'] == 3 
+
+    loc_d3 = parse_loc('DG1.3.1')
+    assert loc_d3['depth'] == 3
+    assert loc_d3['seg'] == 'DG1'
+    assert loc_d3['comp'] == 3 
+    assert loc_d3['subcomp'] == 1
+
+    with pytest.raises(ValueError):
+        parse_loc('DG1')
+    with pytest.raises(ValueError):
+        parse_loc('DG1.2.3.4')
+
+def test_parse_msg_ids():
+    assert parse_msg_id(['PID.3.4', 'PID.3.1', 'PID.18.1'], test_data.msgs) == (
+        test_data.msg_ids
+    )

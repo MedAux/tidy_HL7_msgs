4b87ed1fca296b2f1532da6c5906e79d1958c07c
Mike_Feyder <mfeyder@stellatechnology.com>
2017-09-23 12:15:47 -0400

    refactor for readability

--- query_raw.py
+++ query_raw.py
@@ -1,6 +1,17 @@
+# TODO
+# - check that all fields are from the same segment
+# - sep msg ID into fields 
+# - input args as dictionary of field:col_name
+
+# To test:
+# - pytest: comment out 'import msgs' and main()
+# - example output: keep 'import msgs' and main(); load module; reload module
+#     w/ 'import imp', imp.reload(query_raw) 
+
+from test.test_data import msgs 
+from itertools import repeat
 import re
 import pandas as pd
-# from test.test_data import msgs 
 
 def query_raw(q, store, limit=-1):
     '''
@@ -74,22 +85,22 @@ def flatten(lst):
     '''
     return [item for sublist in lst for item in sublist]
 
-def zip_nested_lists(lst1, lst2):
+def zip_nested(lst1, lst2):
     '''
     Zip nested lists.
 
     Examples:
-        >>> zip_nested_lists([['a', 'b']], [['y', 'z']])
+        >>> zip_nested([['a', 'b']], [['y', 'z']])
         [[('a', 'y'), ('b', 'z')]]
 
-        >>> zip_nested_lists([['a', 'b'],['c', 'd']], [['w', 'x'],['y', 'z']])
+        >>> zip_nested([['a', 'b'],['c', 'd']], [['w', 'x'],['y', 'z']])
         [[('a', 'w'), ('b', 'x')], [('c', 'y'), ('d', 'z')]]
 
     Args:
-        l1: list
-            Length and length of nested lists must be the same length as l2.
-        l2: list
-            Length and length of nested lists must be the same length as l1.
+        lst1: list
+            Length and length of nested lists must be the same length as lst2.
+        lst2: list
+            Length and length of nested lists must be the same length as lst1.
 
     Returns:
         List of nested lists whose elements are tuples.
@@ -97,94 +108,37 @@ def zip_nested_lists(lst1, lst2):
     check_nested_lens_equal(lst1, lst2)
     return [list(zip(lst1[i], lst2[i])) for i in range(len(lst1))]
 
-def concat_fields(lst_parsed):
+def concat(lsts):
     '''
     Examples:
-        >>> concat_fields([[['a', 'b']], [['y', 'z']], [['s', 't']]])
+        >>> concat([[['a', 'b']], [['y', 'z']], [['s', 't']]])
         ['a,y,s', 'b,z,t']
 
-        >>> concat_fields([[['a', 'b']]])
+        >>> concat([[['a', 'b']]])
         ['a', 'b']
 
     '''
-    if len(lst_parsed) == 1 or not bool(lst_parsed[1]):
+    if len(lsts) == 1 or not bool(lsts[1]):
         # either a single parsed field or the appended field is empty, in which
         # case the first element is the concatinated lists
-        return flatten(lst_parsed[0])
+        return flatten(lsts[0])
     else:
-        zipped = zip_nested_lists(lst_parsed[0], lst_parsed[1])
+        zipped = zip_nested(lsts[0], lsts[1])
         concatted = [[",".join(pair) for pair in sublist] for sublist in zipped]
-        to_concat = [concatted, flatten(lst_parsed[2:])]
-        return concat_fields(to_concat)
+        to_concat = [concatted, flatten(lsts[2:])]
+        return concat(to_concat)
 
-def parse_msg_id(fields, msgs):
+def parse_msgs(field_txt, msgs):
     '''
-    Parse message ids from raw HL7 messages.
-
-    The message identifier is a concatination of each field, returning a
-    single string per message. The returned string has the syntax
-    <field_1>,<field_2>,... Its value should be unique as it is used to join
-    different data elements within a message.
-
-    Example:
-        >>> get_msg_id(msgs)
-        ['Facility1,68188,1719801063', Facility2,588229,1721309017', ... ]
-
-    Args:
-        fields: List(string)
-        msgs: List(string)
-
-    Returns:
-        List(string)
+    TODO
     '''
-    fields_parsed = list(map(
-        parse_msgs,
-        fields,
-        [msgs for i in enumerate(fields)]
-    ))
+    return list(map(parse_msg(field_txt), msgs))
 
-    msg_ids = concat_fields(fields_parsed)
-    return msg_ids
-
-def parse_loc(loc_txt):
+def parse_msg(field_txt):
     '''
-    Parse an HL7 message location
+    TODO: problem in past parsing MSH segment?
 
-    Examples:
-        >>> parse_loc('PR1.3')
-        {'seg': 'PR1', 'comp': 3, 'depth': 2}
-
-        >>> parse_loc('DG1.3.1')
-        {'seg': 'DG1', 'comp': 3, 'subcomp': 1, 'depth': 3}
-
-    Args:
-        loc_txt: string
-
-    Returns:
-        Dictionary of attributes and parsed elements of location
-    '''
-    loc = {}
-    loc['depth'] = len(loc_txt.split("."))
-
-    if loc['depth'] not in [2, 3]:
-        raise ValueError(
-            "Syntax of location must be either '<segment>.<component>' or"
-            "'<segment>.<component>.<subcomponent>'"
-        )
-
-    loc_split = loc_txt.split(".")
-
-    loc['seg'] = loc_split[0]
-    loc['comp'] = int(loc_split[1])
-
-    if loc['depth'] == 3:
-        loc['subcomp'] = int(loc_split[2])
-
-    return loc
-
-def parse_el(loc_txt):
-    '''
-    Parse HL7 messages at a given location.
+    Higher-order function to parse a field from an HL7 message
 
     Example:
         >>> msg = '...AL1|3|DA|1545^MORPHINE^99HIC|||20080828|||\n...'
@@ -201,7 +155,9 @@ def parse_el(loc_txt):
         >>> ['MORPHINE', 'CODEINE']
 
     Args:
-        loc (string): HL7 location to be parsed.
+        field: string
+            HL7 field to be parsed.
+
             Must be a single location represented by a segment and 1 or 2
             subsequent components. A period (".") must separate segments and
             components (ex. "PR1.3" or "DG1.3.1")
@@ -209,37 +165,101 @@ def parse_el(loc_txt):
     Returns:
         Function to parse HL7 message at the given location
     '''
-    loc = parse_loc(loc_txt)
+    field = parse_field(field_txt)
 
     def parser(msg):
-        segs = re.findall(loc['seg'] + '\|.*(?=\\n)', msg)
+        segs = re.findall(field['seg'] + '\|.*(?=\\n)', msg)
         data = []
-        # TODO: functional approach
+        # TODO: functional approach; DRY
         for seg in segs:
-            if loc['depth'] == 2:
-                datum = seg.split("|")[loc['comp']]
+            if field['depth'] == 2:
+                datum = seg.split("|")[field['comp']]
             else:
                 try:
-                    comp = seg.split("|")[loc['comp']]
-                    datum = comp.split("^")[loc['subcomp'] - 1]
+                    comp = seg.split("|")[field['comp']]
+                    datum = comp.split("^")[field['subcomp'] - 1]
                 except IndexError:
-                    datum = ''          # empty component (i.e. '||')
+                    # empty component (i.e. '||')
+                    datum = ''
             data.append(datum)
         return data
     return parser
 
+def parse_msg_id(fields, msgs):
+    '''
+    Parse message ids from raw HL7 messages.
+
+    The message identifier is a concatination of each field, returning a
+    single string per message. The returned string has the syntax
+    <field_1>,<field_2>,... Its value should be unique because it is used to
+    join different data elements within a message.
+
+    Example:
+        >>> get_msg_id(msgs)
+        ['Facility1,68188,1719801063', Facility2,588229,1721309017', ... ]
+
+    Args:
+        fields: List(string)
+        msgs: List(string)
+
+    Returns:
+        List(string)
+    '''
+    fields_parsed = list(map(
+        parse_msgs,
+        fields,
+        repeat(msgs)
+    ))
+
+    return concat(fields_parsed)
+
+def parse_field(field_txt):
+    '''
+    Parse an HL7 message field
+
+    Examples:
+        >>> parse_field('PR1.3')
+        {'seg': 'PR1', 'comp': 3, 'depth': 2}
+
+        >>> parse_field('DG1.3.1')
+        {'seg': 'DG1', 'comp': 3, 'subcomp': 1, 'depth': 3}
+
+    Args:
+        loc_txt: string
+
+    Returns:
+        Dictionary of attributes and parsed elements of location
+    '''
+    field = {}
+    field['depth'] = len(field_txt.split("."))
+
+    if field['depth'] not in [2, 3]:
+        raise ValueError(
+            "Syntax of location must be either '<segment>.<component>' or"
+            "'<segment>.<component>.<subcomponent>'"
+        )
+
+    loc_split = field_txt.split(".")
+
+    field['seg'] = loc_split[0]
+    field['comp'] = int(loc_split[1])
+
+    if field['depth'] == 3:
+        field['subcomp'] = int(loc_split[2])
+
+    return field
+
 def zip_msg_ids(lst, msg_ids):
     '''
-    Zip but also ensures list lengths are equal.
+    Zip but ensures list lengths are equal.
     '''
     check_lens_equal(msg_ids, lst)
-    zipped = list(zip(msg_ids, lst))
-    return zipped
+    return list(zip(msg_ids, lst))
 
-def parse_msgs(field, msgs):
-    return list(map(parse_el(field), msgs))
-
-def to_df(lst, field):
+def to_df(lst, field_txt):
+    '''
+    TODO
+    '''
     df = pd.DataFrame.from_dict(
         dict(lst),
         orient="index"
@@ -253,7 +273,7 @@ def to_df(lst, field):
     df.rename(
         columns = {
             "variable": "seg",
-            "value": field
+            "value": field_txt
         },
         inplace=True
     )
@@ -277,18 +297,21 @@ def join_dfs(dfs):
         dfs_to_join.append(df_join)
         return join_dfs(dfs_to_join)
 
-def build_report(fields, msg_ids, msgs):
-    parsed_fields = list(map(
+def parse_report_fields_and_tidy(fields, msg_ids, msgs):
+    '''
+    TODO
+    '''
+    parsed_fields = map(
         parse_msgs,
         fields,
-        [msgs for i in enumerate(fields)]
-    ))
+        repeat(msgs)
+    )
 
-    parsed_fields_w_msg_ids = list(map(
+    parsed_fields_w_msg_ids = map(
         zip_msg_ids,
         parsed_fields,
-        [msg_ids for i in enumerate(fields)]
-    ))
+        repeat(msg_ids)
+    )
 
     field_dfs = list(map(
         to_df,
@@ -308,9 +331,7 @@ def build_report(fields, msg_ids, msgs):
 
 def main():
 
-    # TODO: msg_ids and report fields can be dict of field:col_name
-
-    query_broad = '()s.sending_facility:"COVH"()NOT ()s.attending_doctor_id: IN("1952","2620","2489","1131","1604","3654","2216","1265","2511","3071","3050","3557","746","52118","2507","52715","51071","1080","2716","1948","51644","1321","1066")()s.patient_class:"Inpatient"()s.patient_type: IN("IPB","IPF","IPL","IPM","IPP","IPR","IPS")()d.discharge_time:[2017-08-06 TO 2017-09-20]()s.record_type:"ADT A08"'
+    # query_broad = '()s.sending_facility:"COVH"()NOT ()s.attending_doctor_id: IN("1952","2620","2489","1131","1604","3654","2216","1265","2511","3071","3050","3557","746","52118","2507","52715","51071","1080","2716","1948","51644","1321","1066")()s.patient_class:"Inpatient"()s.patient_type: IN("IPB","IPF","IPL","IPM","IPP","IPR","IPS")()d.discharge_time:[2017-08-06 TO 2017-09-20]()s.record_type:"ADT A08"'
     # msgs = query_raw(query_broad, 'ADTs', 20)
 
     # replace ids w/ msg_id_fields
@@ -320,12 +341,13 @@ def main():
     )
 
     # replace fields w/ report_fields
-    df = build_report(
+    df = parse_report_fields_and_tidy(
         ['DG1.3.1', 'DG1.3.2', 'DG1.6', 'DG1.15'], 
         msg_ids,
         msgs
     )
 
     print(df)
+    return df
 
-# main()
+main()
--- test/test_query_raw.py
+++ test/test_query_raw.py
@@ -9,7 +9,7 @@ def test_lst_lens_equal():
 def test_nested_lst_lens_equal():
     with pytest.raises(RuntimeError):
         check_nested_lens_equal(
-            [[1], [2], [3, 4]], 
+            [[1], [2], [3, 4]],
             [[1], [2], [3, 4, 5]]
         )
 
@@ -19,38 +19,39 @@ def test_flatten():
     assert flatten([[1, 2], []]) == [1, 2]
     assert flatten([]) == []
 
-def test_zip_nested_lsts():
-    assert zip_nested_lists([['a', 'b']], [['y', 'z']]) == (
+def test_zip_nested():
+    assert zip_nested([['a', 'b']], [['y', 'z']]) == (
         [[('a', 'y'), ('b', 'z')]]
     )
-    assert zip_nested_lists([['a', 'b'],['c', 'd']], [['w', 'x'],['y', 'z']]) == (
+    assert zip_nested([['a', 'b'], ['c', 'd']], [['w', 'x'], ['y', 'z']]) == (
         [[('a', 'w'), ('b', 'x')], [('c', 'y'), ('d', 'z')]]
     )
 
 def test_concat_fields():
-    assert concat_fields([[['a', 'b']]]) == ['a', 'b']
-    assert concat_fields([[['a', 'b']], [['y', 'z']], [['s', 't']]]) == (
+    assert concat([[['a', 'b']]]) == ['a', 'b']
+    assert concat([[['a', 'b']], [['y', 'z']], [['s', 't']]]) == (
         ['a,y,s', 'b,z,t']
     )
 
 def test_parse_loc():
-    loc_d2 = parse_loc('PR1.3')
+    loc_d2 = parse_field('PR1.3')
     assert loc_d2['depth'] == 2
     assert loc_d2['seg'] == 'PR1'
-    assert loc_d2['comp'] == 3 
+    assert loc_d2['comp'] == 3
 
-    loc_d3 = parse_loc('DG1.3.1')
+    loc_d3 = parse_field('DG1.3.1')
     assert loc_d3['depth'] == 3
     assert loc_d3['seg'] == 'DG1'
-    assert loc_d3['comp'] == 3 
+    assert loc_d3['comp'] == 3
     assert loc_d3['subcomp'] == 1
 
     with pytest.raises(ValueError):
-        parse_loc('DG1')
+        parse_field('DG1')
     with pytest.raises(ValueError):
-        parse_loc('DG1.2.3.4')
+        parse_field('DG1.2.3.4')
 
 def test_parse_msg_ids():
+    # TODO: test single field
     assert parse_msg_id(['PID.3.4', 'PID.3.1', 'PID.18.1'], test_data.msgs) == (
         test_data.msg_ids
     )

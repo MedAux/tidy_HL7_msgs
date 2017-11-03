c091af22636a4b7d9f10ad920dc04bde9f6262b8
Mike_Feyder <mfeyder@stellatechnology.com>
2017-11-03 13:36:43 -0400

    Update vars name for consistent terminology

--- parsers.py
+++ parsers.py
@@ -7,9 +7,78 @@ import itertools
 import numpy as np
 from .helpers import concat
 
-def get_parser(field):
+def parse_msgs(loc_txt, msgs):
     '''
-    Higher-order function to parse a field from an HL7 message
+    Parse messages for a given location
+
+    Examples:
+        >>> msg1 = '...AL1|3|DA|1545^MORPHINE^99HIC|||20080828|||\n...'
+        >>> msg2 = '...AL1|1|DRUG|00000741^OXYCODONE||HYPOTENSION\n...'
+        >>> parse_msgs("AL1.3.1", [msg1, msg2])
+        >>> [['1545'], ['00000741']]
+
+        >>> # multiple segments per message
+        >>> seg_1 = '...AL1|1|DRUG|00000741^OXYCODONE||HYPOTENSION\n'
+        >>> seg_2 = 'AL1|2|DRUG|00001433^TRAMADOL||SEIZURES~VOMITING\n...'
+        >>> msg3 = seg_1 + seg_2
+        >>> parse_msgs("AL1.3.1", [msg1, msg3])
+        >>> [['1545'], ['00000741', '00001433']]
+
+    Args:
+        loc_txt: string of location to parse
+        msgs: list(string)
+
+    Returns:
+        list(list(string))
+    '''
+    loc = parse_loc_txt(loc_txt)
+    parser = get_parser(loc)
+    return list(map(parser, msgs))
+
+def parse_loc_txt(loc_txt):
+    '''
+    Parse HL7 message location
+
+    Examples:
+        >>> parse_loc_txt('PR1.3')
+        {'seg': 'PR1', 'field': 3, 'depth': 2}
+
+        >>> parse_loc_txt('DG1.3.1')
+        {'seg': 'DG1', 'field': 3, 'comp': 0, 'depth': 3}
+
+    Args:
+        loc_txt: string of location
+
+    Returns:
+        Dictionary of location attributes and parsed elements
+
+    Raises:
+        ValueError if location syntax is incorrect
+    '''
+    loc = {}
+    loc_split = loc_txt.split(".")
+    loc['depth'] = len(loc_split)
+
+    if loc['depth'] not in [2, 3]:
+        raise ValueError(
+            "Syntax of location must be either '<segment>.<field>' or "
+            "'<segment>.<field>.<component>'"
+        )
+
+    loc['seg'] = loc_split[0]
+    loc['field'] = int(loc_split[1])
+
+    if loc['seg'] == "MSH":
+        loc['field'] -= 1
+
+    if loc['depth'] == 3:
+        loc['comp'] = int(loc_split[2]) - 1
+
+    return loc
+
+def get_parser(loc):
+    '''
+    Higher-order function to parse a location from an HL7 message
 
     Example:
         >>> msg = '...AL1|3|DA|1545^MORPHINE^99HIC|||20080828|||\n...'
@@ -28,11 +97,10 @@ def get_parser(field):
         >>> ['MORPHINE', 'CODEINE']
 
     Args:
-        field: dict
-            Field attributes and values
+        loc: dict of location attributes and values
 
     Returns:
-        Function to parse an HL7 message at a given field
+        Function to parse an HL7 message at a given location
     '''
     def parser(msg):
         '''
@@ -44,156 +112,77 @@ def get_parser(field):
         Returns:
             list(string)
         '''
-        assert field['depth'] in [2, 3]
+        assert loc['depth'] in [2, 3]
 
-        field_sep = list(msg)[3]
-        comp_sep = list(msg)[4]
+        field_sep, comp_sep = list(msg)[3:5]
 
-        seg_regex = field['seg'] + re.escape(field_sep) + '.*(?=\\n)'
-        segs = re.findall(seg_regex, msg)
+        seg_re = loc['seg'] + re.escape(field_sep) + '.*(?=\\n)'
+        segs = re.findall(seg_re, msg)
 
         data = []
         for seg in segs:
             seg_split = seg.split(field_sep)
-            if field['depth'] == 2:
+            if loc['depth'] == 2:
                 try:
-                    comp = seg_split[field['comp']]
-                    data.append(comp)
+                    field_val = seg_split[loc['field']]
+                    data.append(field_val)
                 except IndexError:
                     data.append(np.nan)
             else:
                 try:
-                    comp = seg_split[field['comp']]
-                    subcomp = comp.split(comp_sep)[field['subcomp']]
-                    data.append(subcomp)
+                    field_val = seg_split[loc['field']]
+                    comp_val = field_val.split(comp_sep)[loc['comp']]
+                    data.append(comp_val)
                 except IndexError:
                     data.append(np.nan)
         return data
     return parser
 
-def parse_field_txt(field_txt):
+def parse_msg_id(id_locs_txt, msgs):
     '''
-    Parse literal HL7 message field (i.e. 'DG1.3.1')
+    Parse message IDs from raw HL7 messages
 
-    Examples:
-        >>> parse_field_txt('PR1.3')
-        {'seg': 'PR1', 'comp': 3, 'depth': 2}
-
-        >>> parse_field_txt('DG1.3.1')
-        {'seg': 'DG1', 'comp': 3, 'subcomp': 0, 'depth': 3}
-
-    Args:
-        field_txt: string
-            Field name
-
-    Returns:
-        Dictionary of field attributes and parsed elements
-
-    Raises:
-        ValueError if field syntax is incorrect
-    '''
-    field = {}
-    field['depth'] = len(field_txt.split("."))
-
-    if field['depth'] not in [2, 3]:
-        raise ValueError(
-            "Syntax of location must be either '<segment>.<field>' or"
-            "'<segment>.<field>.<component>'"
-        )
-
-    loc_split = field_txt.split(".")
-
-    field['seg'] = loc_split[0]
-    field['comp'] = int(loc_split[1])
-
-    if field['seg'] == "MSH":
-        field['comp'] -= 1
-
-    if field['depth'] == 3:
-        field['subcomp'] = int(loc_split[2]) - 1
-
-    return field
-
-def parse_msg_id(fields, msgs):
-    '''
-    Parse message IDs from raw HL7 messages.
-
-    The message identifier is a concatination of each field value, which must
-    be a single value for each message (i.e. the field must be for a segment
-    only is found once in a message). Returns a single string per message.
-    The returned string has the syntax <field_1>,<field_2>,... Its value must
-    be unique because it is used to join different data elements within a
-    message.
+    The message identifier is a concatination of each ID location, which must
+    be a single value for each message (i.e. the location must be for a
+    segment found only once in a message). Returns a single string per
+    message. Its value must be unique for each message because it is used to
+    join data elements within a message.
 
     Example:
-        >>> get_msg_id(msgs)
+        >>> parse_msg_id(['MSH.7', 'PID.3.1', 'PID.3.4'], msgs)
         ['Facility1,68188,1719801063', Facility2,588229,1721309017', ... ]
 
     Args:
-        fields: list(string)
+        id_locs_txt: list(string)
         msgs: list(string)
 
     Returns:
         list(string)
 
     Raises:
-        RuntimeError if a field has multiple values
+        RuntimeError if a location has multiple values
         RuntimeError if message IDs are not unique
     '''
-    parsed_fields = list(map(
+    ids_per_loc = list(map(
         parse_msgs,
-        fields,
+        id_locs_txt,
         itertools.repeat(msgs)
     ))
 
-    # NOTE: too restrictive?
-    is_field_single_ids = (
-        [all([len(ids) == 1 for ids in field]) for field in parsed_fields]
+    are_loc_ids_single = (
+        [all([len(id_val) == 1 for id_val in loc_ids]) for loc_ids in ids_per_loc]
     )
 
-    if not all(is_field_single_ids):
+    if not all(are_loc_ids_single):
         raise RuntimeError(
-            "One or more ID fields have multiple values per message: {fields}. ".format(
-                fields=", ".join(itertools.compress(fields, is_field_single_ids))
+            "One or more ID locations have multiple values per message: {id_locs}. ".format(
+                id_locs=", ".join(itertools.compress(id_locs_txt, are_loc_ids_single))
             )
         )
 
-    concatted = concat(parsed_fields)
+    concatted = concat(ids_per_loc)
 
     if len(set(concatted)) != len(msgs):
         raise RuntimeError("Messages IDs are not unique")
 
     return concatted
-
-def parse_msgs(field_txt, msgs):
-    '''
-    Parse messages for a given field
-
-    Examples:
-        >>> msg1 = '...AL1|3|DA|1545^MORPHINE^99HIC|||20080828|||\n...'
-        >>> msg2 = '...AL1|1|DRUG|00000741^OXYCODONE||HYPOTENSION\n...'
-        >>> parse_msgs("AL1.3.1", [msg1, msg2])
-        >>> [['1545'], ['00000741']]
-
-        >>> # multiple segments per message
-        >>> seg_1 = '...AL1|1|DRUG|00000741^OXYCODONE||HYPOTENSION\n'
-        >>> seg_2 = 'AL1|2|DRUG|00001433^TRAMADOL||SEIZURES~VOMITING\n...'
-        >>> msg3 = seg_1 + seg_2
-        >>> parse_msgs("AL1.3.1", [msg1, msg3])
-        >>> [['1545'], ['00000741', '00001433']]
-
-    Args:
-        field_txt: string
-            Field to parse. Must be a single location represented by a
-            segment, a component and an optional subcomponent. A period (".")
-            must separate segments, components, and subcomponents (ex.
-            "PR1.3" or "DG1.3.1")
-
-        msgs: list(string)
-
-    Returns:
-        list(list(string))
-    '''
-    field = parse_field_txt(field_txt)
-    parser = get_parser(field)
-    return list(map(parser, msgs))

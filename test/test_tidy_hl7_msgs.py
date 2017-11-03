ecd87dd912443bc067a245948e980eb5787843d0
Mike_Feyder <mfeyder@stellatechnology.com>
2017-11-03 13:38:23 -0400

    Update testing for new var names

--- test/test_tidy_hl7_msgs.py
+++ test/test_tidy_hl7_msgs.py
@@ -7,7 +7,7 @@ Run 'pytest -s' to print df
 
 import pytest
 from ..tidy_hl7_msgs import tidy_segs
-from ..parsers import parse_field_txt, parse_msgs, parse_msg_id
+from ..parsers import parse_loc_txt, parse_msgs, parse_msg_id
 from ..helpers import (
     concat, flatten, zip_nested, are_lens_equal,
     are_segs_identical, are_nested_lens_equal,
@@ -48,6 +48,13 @@ def test_are_nested_lens_equal():
         [[1], [1], [2, 2]]
     ) is False
 
+def test_are_segs_identical():
+    identical_segs = ['DG1.3.1', 'DG1.3.2', 'DG1.6']
+    assert are_segs_identical(identical_segs) is True
+
+    non_identical_segs = ['DG1.3.1', 'DG1.3.2', 'PID.3.4']
+    assert are_segs_identical(non_identical_segs) is False
+
 def test_flatten():
     assert flatten([[1, 2], [3, 4]]) == [1, 2, 3, 4]
     assert flatten([[1, 2, [3, 4]]]) == [1, 2, [3, 4]]
@@ -81,6 +88,29 @@ def test_parse_msgs():
         ['M43.16', 'M43.16', 'I10', 'M48.06']
     ]
 
+def test_parse_loc_txt():
+    field_d2 = parse_loc_txt('PR1.3')
+    assert field_d2['depth'] == 2
+    assert field_d2['seg'] == 'PR1'
+    assert field_d2['field'] == 3
+
+    field_d3 = parse_loc_txt('DG1.3.1')
+    assert field_d3['depth'] == 3
+    assert field_d3['seg'] == 'DG1'
+    assert field_d3['field'] == 3
+    assert field_d3['comp'] == 0
+
+    msh_d3 = parse_loc_txt('MSH.3.1')
+    assert msh_d3['depth'] == 3
+    assert msh_d3['seg'] == 'MSH'
+    assert msh_d3['field'] == 2
+    assert msh_d3['comp'] == 0
+
+    with pytest.raises(ValueError):
+        parse_loc_txt('DG1')
+    with pytest.raises(ValueError):
+        parse_loc_txt('DG1.2.3.4')
+
 def test_parse_msg_ids():
     assert parse_msg_id(['PID.3.1', 'PID.3.4', 'MSH.7'], TEST_MSGS) == (
         ['123,FACILITY A,20170515104040', '456,FACILITY B,20170711123256']
@@ -94,56 +124,16 @@ def test_parse_msg_ids():
     with pytest.raises(RuntimeError):
         parse_msg_id(['PID.3.1', 'PID.3.4'], non_unique_msg_ids)
 
-
-def test_parse_field_txt():
-    field_d2 = parse_field_txt('PR1.3')
-    assert field_d2['depth'] == 2
-    assert field_d2['seg'] == 'PR1'
-    assert field_d2['comp'] == 3
-
-    field_d3 = parse_field_txt('DG1.3.1')
-    assert field_d3['depth'] == 3
-    assert field_d3['seg'] == 'DG1'
-    assert field_d3['comp'] == 3
-    assert field_d3['subcomp'] == 0
-
-    msh_d3 = parse_field_txt('MSH.3.1')
-    assert msh_d3['depth'] == 3
-    assert msh_d3['seg'] == 'MSH'
-    assert msh_d3['comp'] == 2
-    assert msh_d3['subcomp'] == 0
-
-    with pytest.raises(ValueError):
-        parse_field_txt('DG1')
-    with pytest.raises(ValueError):
-        parse_field_txt('DG1.2.3.4')
-
-def test_are_segs_identical():
-    identical_segs = ['DG1.3.1', 'DG1.3.2', 'DG1.6']
-    assert are_segs_identical(identical_segs) is True
-
-    non_identical_segs = ['DG1.3.1', 'DG1.3.2', 'PID.3.4']
-    assert are_segs_identical(non_identical_segs) is False
-
 def test_tidy_segs():
     # pylint: disable=invalid-name
 
-    # id_fields_lst = ['PID.3.4', 'PID.3.1', 'PID.18.1']
-    # report_fields_lst = ['DG1.3.1', 'DG1.3.2', 'DG1.6', 'DG1.15'],
-
-    # df = tidy_HL7_msg_segs(
-    #     id_fields_lst,
-    #     report_fields_lst,
-    #     test_data.msgs
-    # )
-
-    id_fields_dict = {
+    ids_locs = {
         'MSH.7': 'id_field_1',
         'PID.3.1': 'id_field_2',
         'PID.3.4': 'id_field_3',
     }
 
-    report_fields_dict = {
+    report_locs = {
         'DG1.3.1': 'report_field_1',
         'DG1.3.2': 'report_field_2',
         'DG1.3.3': 'report_field_3',
@@ -152,16 +142,17 @@ def test_tidy_segs():
     }
 
     df = tidy_segs(
-        id_fields_dict,
-        report_fields_dict,
+        ids_locs,
+        report_locs,
         TEST_MSGS
     )
 
-    col_names = list(id_fields_dict.values()) + list(report_fields_dict.values())
-    assert all([col_name in df.columns.values for col_name in col_names]) is True
-
     print('\n')
     print(df)
+
+    # columns renamed
+    col_names = list(ids_locs.values()) + list(report_locs.values())
+    assert all([col in df.columns.values for col in col_names]) is True
 
     # segments of report fields not the same
     with pytest.raises(ValueError):

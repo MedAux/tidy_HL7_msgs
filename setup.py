0f5a018ceade096110cd2dc0cd13743e3b244d58
Mike_Feyder <mfeyder@stellatechnology.com>
2017-11-04 15:39:27 -0400

    Finish setup

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
+'''
+Setup
+'''
+
 from setuptools import setup
 
-
 setup(
-    name='Tidy HL7 Messages',
+    name='tidy_hl7_msgs',
     version='0.1.0',
     description='A simply utility to parse and tidy HL7 messages',
-    # url='',
+    url='https://github.com/feyderm/tidy_HL7_msgs.git',
     author='Michael Feyder',
     author_email='feyderm@gmail.com',
-    # license='',
+    license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Healthcare Industry',
-        # 'License :: OSI Approved :: MIT License'
-        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License',
     ],
     keywords='healthcare HL7',
     packages=['tidy_hl7_msgs'],

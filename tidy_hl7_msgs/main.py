642b566af930b1255108f5d97372d8e181ac8839
Mike_Feyder <mfeyder@stellatechnology.com>
2017-11-09 12:24:36 -0500

    Update documentation and condense multi-line args

--- tidy_hl7_msgs/main.py
+++ tidy_hl7_msgs/main.py
@@ -1,61 +1,60 @@
 '''
-Tidy HL7 message segments
+Tidy HL7 v2 message segments
 '''
 
 import itertools
 import pandas as pd
 from tidy_hl7_msgs.helpers import (
-    to_df,
-    join_dfs,
-    zip_msg_ids,
-    are_segs_identical,
+    to_df, join_dfs, zip_msg_ids, are_segs_identical
 )
-from tidy_hl7_msgs.parsers import (
-    parse_msgs,
-    parse_msg_id
-)
+from tidy_hl7_msgs.parsers import parse_msgs, parse_msg_id
 
 def tidy_segs(msg_id_locs, report_locs, msgs):
     ''' Tidy HL7 message segments
 
     Parameters
     ----------
-    msg_id_locs : list or able to be converted to one
+    msg_id_locs : list or dict
 
-        Locations (i.e. HL7 message fields and/or components) that in
-        combination uniquely identify a message. Locations can be from
-        different message segments, but each location must return one
-        value per message. Message IDs must uniquely identify messages.
+        Locations (i.e. HL7 message fields or components) that taken together
+        uniquely identify messages after de-duplication. Locations can be
+        from different message segments, but each location must return one
+        value per message. Values must not be missing. Message IDs must
+        uniquely identify messages.
 
-        Location syntax must be either '<segment>.<field>' or
-        '<segment>.<field>.<component>', delinated by a period ('.')
-        (ex. 'MSH.4' or 'MSH.4.1')
+        Location syntax must be either <segment>.<field> or
+        <segment>.<field>.<component>, delinated by a period (ex. 'MSH.4'
+        or 'MSH.4.1')
 
-        If passed a dictionary, its keys will be parsed locations and its
-        values will be column names.
+        If passed a dictionary, its keys must be ID locations and its values
+        will be corresponding column names in the returned dataframe.
 
-    report_loc : list or able to be converted to one
+    report_loc : list or dict
 
-        Locations (i.e. fields and/or components) to report. Locations
-        must be from the same segment.
+        Locations (i.e. HL7 message fields or components) to report.
+        Locations must be from the same segment.
 
-        Location syntax must be either '<segment>.<field>' or
-        '<segment>.<field>.<component>', delinated by a period ('.')
-        (ex. 'DG1.2' or 'DG1.2.1')
+        Location syntax must be either <segment>.<field> or
+        <segment>.<field>.<component>, delinated by a period (ex. 'DG1.4'
+        or 'DG1.4.1')
 
-        If passed a dictionary, its keys will be parsed locations and its
-        values will be column names.
+        If passed a dictionary, its keys must be report locations and its
+        values will be corresponding column names in the returned dataframe.
 
-    msgs : list(string) of HL7 messages
+    msgs : list(string) of HL7 v2 messages
 
     Returns
     -------
     Dataframe
 
-        For each unique message, rows for each segment and columns for
-        message ids, segment number, and report locations.
+        Columns: one for each message ID/report location and for segment number
 
-        Missing values are reported as numpy NaN.
+        Rows: one per segment
+
+            Missing values are reported as NAs
+
+            If message is missing segment, a single row for this message is
+            returned with a segment number of NA and NAs for report locations.
 
     Raises
     ------
@@ -81,25 +80,13 @@ def tidy_segs(msg_id_locs, report_locs, msgs):
     msg_ids = parse_msg_id(list(msg_id_locs), msgs_unique)
 
     # parse report locations
-    report_vals = map(
-        parse_msgs,
-        list(report_locs),
-        itertools.repeat(msgs_unique)
-    )
+    report_vals = map(parse_msgs, list(report_locs), itertools.repeat(msgs_unique))
 
     # zip values for each report location w/ message ids
-    zipped = map(
-        zip_msg_ids,
-        report_vals,
-        itertools.repeat(msg_ids)
-    )
+    zipped = map(zip_msg_ids, report_vals, itertools.repeat(msg_ids))
 
-    # convert each zipped reported value + message id to a dataframe
-    dfs = list(map(
-        to_df,
-        zipped,
-        report_locs
-    ))
+    # convert each zipped message id + report value to a dataframe
+    dfs = list(map(to_df, zipped, report_locs))
 
     # join dataframes
     df = join_dfs(dfs)
@@ -113,9 +100,13 @@ def tidy_segs(msg_id_locs, report_locs, msgs):
     id_cols.columns = msg_id_locs
     df = pd.concat([id_cols, df], axis=1).drop(['msg_id'], axis=1)
 
-    # rename cols if locs are dicts
+    # rename columns if locs are dicts
     try:
         df.rename(columns=msg_id_locs, inplace=True)
+    except TypeError:
+        pass
+
+    try:
         df.rename(columns=report_locs, inplace=True)
     except TypeError:
         pass
